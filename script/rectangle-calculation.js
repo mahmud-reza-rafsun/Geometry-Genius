676958b444304cad67ad0cba449874b01141cdbe
Mahmud Reza Rafsun <97410422+mahmud-reza-rafsun@users.noreply.github.com>
2024-03-29 02:10:51 +0600

    added parallelogram calculation function and design

--- script/rectangle-calculation.js
+++ script/rectangle-calculation.js
@@ -1,3 +1,21 @@
 function calculateRectangleArea() {
-    console.log('hello click')
+    // get width of the rectangle
+    const widthInput = document.getElementById('rectangle-width');
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText);
+    console.log(width);
+
+    // get length of the rectangle
+    const lengthInput = document.getElementById('rectangle-length');
+    const lenghtText = lengthInput.value;
+    const length = parseFloat(lenghtText);
+    console.log(length);
+
+    // calculate rectangle area
+    const area = width * length;
+    console.log('area of the rectangle is:', area);
+
+    // display rectangle area
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
 }
